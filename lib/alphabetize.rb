61fa9d384e59a2bba4368fdf433fed0c22bd6cd3
Carson Rey <c.rey494@gmail.com>
2018-10-06 15:56:19 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -9,3 +9,7 @@ def alphabetize(arr)
     end
   end
 end
+
+# don't need to iterate down to 'letter' from each word in the string. '.split("")' takes care of splitting each word into letters.
+
+#'.split' on its own will default to seperating words in a sentence, but given an argument of '("")' it will split each element(word) with no space in between as well as the space between words.
