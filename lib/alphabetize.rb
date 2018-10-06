c9ea5924b6b02d44da6c126921075d53c4ae19d3
Carson Rey <c.rey494@gmail.com>
2018-10-06 15:38:44 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,7 +5,7 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   arr.sort_by do |string|
     string.split.collect do |word|
-      word.split.collect.sort_by do |letter| 
+      word.split("").collect.sort_by do |letter| 
         binding.pry 
         ESPERANTO_ALPHABET.index(letter)
       end
