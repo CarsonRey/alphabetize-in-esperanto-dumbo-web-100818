880dd459e306959ba784d3401694f72ec950f309
Carson Rey <c.rey494@gmail.com>
2018-10-06 15:26:39 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,7 +5,7 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   arr.sort_by do |string|
     string.split.collect do |letter|
-      letter.sort_by { |letter| ESPERANTO_ALPHABET.index(letter)
+      letter.sort_by { |letter| ESPERANTO_ALPHABET.index(letter) }
     end
   end
 end
